3ebef3c2c68db0c939f275e838cffabaf5b93da7
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-08-17 17:26:56 +0300

    adding attributes to goals migration

--- database/migrations/2024_08_17_141326_create_goals_table.php
+++ database/migrations/2024_08_17_141326_create_goals_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('goals', function (Blueprint $table) {
             $table->id();
+            $table->enum('type',['finance','health','education']);
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate("cascade");
             $table->timestamps();
         });
     }
