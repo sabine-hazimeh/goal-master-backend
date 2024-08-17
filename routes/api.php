24975ce949f941d55ed25be8a43766e8e03717f1
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-08-17 20:12:01 +0300

    adding routes for finance goals

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FinanceGoalController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
@@ -11,3 +12,4 @@ Route::middleware('checkAuth')->group(function () {
     Route::get('/logout', [AuthController::class, 'logout']);
    
 });
+Route::apiResource('finance', FinanceGoalController::class);
