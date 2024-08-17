38336eb41601dd3d501f2a62be28c180462fb208
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-08-17 13:12:30 +0300

    updating run method in userseeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use App\Models\User;
 class UserSeeder extends Seeder
 {
     /**
@@ -12,6 +12,6 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+       User::factory(50)->create();
     }
 }
