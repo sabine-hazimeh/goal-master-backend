0bedc435aa20d255c240d84bbba812c817efef29
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-08-17 19:00:58 +0300

    adding finance goal factory

--- database/factories/FinanceGoalFactory.php
+++ database/factories/FinanceGoalFactory.php
@@ -3,7 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-
+use App\Models\FinanceGoal;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\FinanceGoal>
  */
@@ -17,7 +17,13 @@ class FinanceGoalFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'income' => $this->faker->numberBetween(1000, 5000),
+            'savings' => $this->faker->numberBetween(100, 1000),
+            'expenses' => $this->faker->numberBetween(500, 4500),
+            'target' => $this->faker->numberBetween(10000, 50000),
+            'target_date' => $this->faker->date(),
+            'goal_id' => Goal::inRandomOrder()->first()->id,
+
         ];
     }
 }
