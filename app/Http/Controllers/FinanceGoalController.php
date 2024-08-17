a3aa885a86585b23f8522bba231ef1b97fc8f1c5
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-08-17 18:34:00 +0300

    adding finance goal model

---
+++ app/Http/Controllers/FinanceGoalController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\FinanceGoal;
+use App\Http\Requests\StoreFinanceGoalRequest;
+use App\Http\Requests\UpdateFinanceGoalRequest;
+
+class FinanceGoalController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreFinanceGoalRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(FinanceGoal $financeGoal)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(FinanceGoal $financeGoal)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateFinanceGoalRequest $request, FinanceGoal $financeGoal)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(FinanceGoal $financeGoal)
+    {
+        //
+    }
+}
---
+++ database/seeders/FinanceGoalSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class FinanceGoalSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
