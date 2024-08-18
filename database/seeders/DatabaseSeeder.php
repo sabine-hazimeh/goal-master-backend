06c13ba7f6e30c900ffd604ab3baba95705a0333
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-08-18 12:34:55 +0300

    registring health goal seeder in database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,7 +22,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             GoalSeeder::class,
-            FinanceGoalSeeder::class
+            FinanceGoalSeeder::class,
+            HealthGoalSeeder::class
         ]);   
     }
 }
