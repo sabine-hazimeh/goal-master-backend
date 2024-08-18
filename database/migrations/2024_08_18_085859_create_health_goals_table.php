c954d16565749372340899a0ccb0f911e9cedc1a
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-08-18 12:10:05 +0300

    adding health goal migration

--- database/migrations/2024_08_18_085859_create_health_goals_table.php
+++ database/migrations/2024_08_18_085859_create_health_goals_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('health_goals', function (Blueprint $table) {
             $table->id();
+            $table->integer('age');
+            $table->enum('gender', ['male', 'female']);
+            $table->decimal('height', 5, 2);
+            $table->decimal('current_weight', 5, 2);
+            $table->decimal('desired_weight', 5, 2);
+            $table->string('medical_conditions');
+            $table->date('time_horizon');
             $table->timestamps();
         });
     }
