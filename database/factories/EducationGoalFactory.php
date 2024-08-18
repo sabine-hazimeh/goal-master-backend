42e14aa55ae4db8fa2a8ef8c742323ffc5671e18
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-08-18 14:58:44 +0300

    creating factory for education goal

--- database/factories/EducationGoalFactory.php
+++ database/factories/EducationGoalFactory.php
@@ -3,7 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-
+use App\Models\Goal;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\EducationGoal>
  */
@@ -17,7 +17,13 @@ class EducationGoalFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'goal' => $this->faker->sentence(),
+            'current_knowledge' => $this->faker->sentence(),
+            'available_days' => $this->faker->numberBetween(1, 7),
+            'available_hours' => $this->faker->numberBetween(1, 24),
+            'time_horizon' => $this->faker->date(),
+            'goal_id' => Goal::inRandomOrder()->first()->id,
         ];
+        
     }
 }
