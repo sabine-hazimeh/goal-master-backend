60e8f5fae8178140b62e4f1652fb18c368c7914c
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-08-18 12:39:15 +0300

    fixing imports error

--- database/factories/HealthGoalFactory.php
+++ database/factories/HealthGoalFactory.php
@@ -3,6 +3,8 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Goal;
+use App\Models\HealthGoal;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\HealthGoal>
@@ -17,12 +19,12 @@ class HealthGoalFactory extends Factory
     public function definition(): array
     {
         return [
-            'age'-> $this->faker->numberBetween(1, 100),
+            'age'=> $this->faker->numberBetween(1, 100),
             'gender' => $this->faker->randomElement(['male', 'female']),
             'height' => $this->faker->numberBetween(1, 200),
             'current_weight' => $this->faker->numberBetween(1, 150),
             'desired_weight' => $this->faker->numberBetween(1, 100),
-            'medical_condition' => $this->faker->sentence(),
+            'medical_conditions' => $this->faker->sentence(),
             'time_horizon' => $this->faker->date(),
             'goal_id' => Goal::inRandomOrder()->first()->id,
 
