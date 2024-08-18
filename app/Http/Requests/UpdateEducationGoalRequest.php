ab71efa525276d15e3ac0dcb08ed1fa9c1cc6bfc
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-08-18 19:06:56 +0300

    adding validation for update attributes

--- app/Http/Requests/UpdateEducationGoalRequest.php
+++ app/Http/Requests/UpdateEducationGoalRequest.php
@@ -11,7 +11,7 @@ class UpdateEducationGoalRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,11 @@ class UpdateEducationGoalRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'goal' => 'sometimes|string|max:255',
+            'current_knowledge' => 'sometimes|string|max:255',
+            'available_days' => 'sometimes|integer|min:1|max:7', 
+            'available_hours' => 'sometimes|integer|min:1|max:24',
+            'time_horizon' => 'sometimes|date',
         ];
     }
 }
