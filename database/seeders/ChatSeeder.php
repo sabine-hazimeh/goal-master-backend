b6e10c6c540998fa76fb9de0f5dcdc9f26431698
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-08-19 11:03:20 +0300

    adding seeder for chats

--- database/seeders/ChatSeeder.php
+++ database/seeders/ChatSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use App\Models\Chat;
 class ChatSeeder extends Seeder
 {
     /**
@@ -12,6 +12,6 @@ class ChatSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Chat::factory(50)->create();
     }
 }
