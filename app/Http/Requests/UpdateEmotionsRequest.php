e99379f4ea3f1c0f651dc23187739247ac4b19c8
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-08-19 21:59:39 +0300

    adding validation for attributes when updating emotions

--- app/Http/Requests/UpdateEmotionsRequest.php
+++ app/Http/Requests/UpdateEmotionsRequest.php
@@ -11,7 +11,7 @@ class UpdateEmotionsRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,8 @@ class UpdateEmotionsRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            "type"=>"sometimes|in:manual,detected",
+            "emotion"=>"sometimes|string",
         ];
     }
 }
