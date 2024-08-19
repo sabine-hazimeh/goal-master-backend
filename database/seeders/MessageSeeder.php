b3558080ad9e7e1861ff420c6b98c864723b4763
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-08-19 14:45:53 +0300

    adding seeder for messages

--- database/seeders/MessageSeeder.php
+++ database/seeders/MessageSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use App\Models\Message;
 class MessageSeeder extends Seeder
 {
     /**
@@ -12,6 +12,6 @@ class MessageSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Message::factory(50)->create();
     }
 }
