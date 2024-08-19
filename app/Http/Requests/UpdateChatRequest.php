30af2da98fa1d70666be420efd6aae17466daef3
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-08-19 12:45:16 +0300

    adding validation for updating chat attributes

--- app/Http/Requests/UpdateChatRequest.php
+++ app/Http/Requests/UpdateChatRequest.php
@@ -11,7 +11,7 @@ class UpdateChatRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,8 @@ class UpdateChatRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            "user_id" => "sometimes|exists:users,id",
+            "consultant_id" => "sometimes|exists:users,id",
         ];
     }
 }
