6fd00e058ff136722e1e0ed2e9ff5386aaf12b19
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-08-19 21:48:57 +0300

    adding validation on attributes for adding emotions

--- app/Http/Requests/StoreEmotionsRequest.php
+++ app/Http/Requests/StoreEmotionsRequest.php
@@ -11,7 +11,7 @@ class StoreEmotionsRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,8 @@ class StoreEmotionsRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            "type"=>"required|in:manual,detected",
+            "emotion"=>"required|string",
         ];
     }
 }
