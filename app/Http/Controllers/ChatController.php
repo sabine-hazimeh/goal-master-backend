42595b6a177a140f892ed8e144d61e7de819c684
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-08-19 12:42:13 +0300

    adding store method for chats

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -18,19 +18,12 @@ class ChatController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(StoreChatRequest $request)
     {
-        //
+        $chat = Chat::create($request->validated());
+        return response()->json(["chat" => $chat], 201);
     }
 
     /**
