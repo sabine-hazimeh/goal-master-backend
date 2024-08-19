499d5bdad00a9713a083f693f96678e51cbc9523
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-08-19 16:54:11 +0300

    adding validation for attributes when updating message

--- app/Http/Requests/UpdatemessageRequest.php
+++ app/Http/Requests/UpdatemessageRequest.php
@@ -11,7 +11,7 @@ class UpdatemessageRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,9 @@ class UpdatemessageRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            "sender_id" => "sometimes|exists:users,id",
+            "chat_id" => "sometimes|exists:chats,id",
+            "content" => "sometimes|string|max:255",
         ];
     }
 }
