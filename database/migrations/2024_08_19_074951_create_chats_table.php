66e923f0294f8171fea044b52d0738a55ba5e0e8
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-08-19 10:54:22 +0300

    adding chat migration

--- database/migrations/2024_08_19_074951_create_chats_table.php
+++ database/migrations/2024_08_19_074951_create_chats_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('chats', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('consultant_id')->constrained('users')->onDelete('cascade')->onUpdate("cascade");
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade')->onUpdate("cascade");
             $table->timestamps();
         });
     }
