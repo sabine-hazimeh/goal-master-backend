ecc742ac5f75f7414e977fd5a619c6ac2adbada4
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-08-19 14:36:23 +0300

    adding fillable for message model

--- app/Models/message.php
+++ app/Models/message.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class message extends Model
 {
     use HasFactory;
+    protected $fillable=[
+        'sender_id',
+        'chat_id',
+        'content',
+    ];
 }
