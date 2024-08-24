50f2d577dc69bfc47f268490465ea7b7144f2fc1
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-08-24 21:20:55 +0300

    adding consultant middleware

---
+++ app/Http/Middleware/ConsultantsMiddleware.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+class ConsultantsMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $user = Auth::user();
+        
+        if ($user->role != "consultant") {
+            return response()->json([
+                "message" => "unauthorized"
+            ], 403); 
+        }
+
+        return $next($request);
+    }
+}
