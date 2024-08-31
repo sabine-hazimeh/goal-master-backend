081ff2e034a7beac785c52896873a543386381f5
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-08-31 21:08:47 +0300

    adding route for coursera controller

--- routes/api.php
+++ routes/api.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\ChatController;
 use App\Http\Controllers\MessageController;
 use App\Http\Controllers\EmotionsController;
 use App\Http\Controllers\JournalsController;
+use App\Http\Controllers\CourseraController;
 
 Route::post('/register', [AuthController::class, 'registerUser']);
 Route::post('/register/consultant', [AuthController::class, 'registerConsultant'])->middleware(['auth:api', 'admin']);
@@ -29,11 +30,11 @@ Route::middleware(['auth:api', 'checkAuth'])->group(function () {
     Route::apiResource('message', MessageController::class);
     Route::apiResource('emotions', EmotionsController::class);
     Route::apiResource('journal', JournalsController::class);
-   
     Route::get('/messages/{chat_id}', [MessageController::class, 'getMessagesByChatId']);
     Route::get('/user-journals', [JournalsController::class, 'userJournals']);
     Route::post('/chat', [ChatController::class, 'getOrCreateChat']);
     Route::get('/chat/{chat_id}/messages', [ChatController::class, 'getMessages']);
+    Route::apiResource('coursera', CourseraController::class);
 });
 
 Route::get('users', [AuthController::class, 'DisplayUsers'])->middleware(['auth:api', 'consultants']);
