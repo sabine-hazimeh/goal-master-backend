650549bbb44a7ac4975601c9732819d9fde011c2
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-08-31 21:01:15 +0300

    adding seeder for coursera

--- database/seeders/CourseraSeeder.php
+++ database/seeders/CourseraSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use App\Models\Coursera;
 class CourseraSeeder extends Seeder
 {
     /**
@@ -12,6 +12,6 @@ class CourseraSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Coursera::factory(50)->create();
     }
 }
